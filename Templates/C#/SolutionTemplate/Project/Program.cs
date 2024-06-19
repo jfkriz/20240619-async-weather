c512268a469e458feec650284a2442115b3f00dc
charlie retzler <charlie.retzler@gmail.com>
2024-06-19 08:54:33 -0400

    Added async main to C# main method

--- Templates/C#/SolutionTemplate/Project/Program.cs
+++ Templates/C#/SolutionTemplate/Project/Program.cs
@@ -7,7 +7,9 @@ namespace Project
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Hello World! {DateTime.Now.ToLongTimeString()}");
+            await Task.Delay(10 * 1000);
+            Console.WriteLine($"And Goodbye too! {DateTime.Now.ToLongTimeString()}");
         }
     }
 }
